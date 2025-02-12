28894d67eb36c7a51e7b28cedfb60f028a377b53
Luis Sago <lws.sago@gmail.com>
2025-02-12 03:41:43 +0100

    Simplify count, just return number of rows

--- v6_basic_stats_py/__init__.py
+++ v6_basic_stats_py/__init__.py
@@ -5,22 +5,19 @@ from vantage6.algorithm.tools.decorators import data
 
 
 @data(1)
-def partial_basic_stats(df: pd.DataFrame, column_name: str):
+def partial_basic_stats(df: pd.DataFrame):
     """Compute the basic stats partial
 
     The data argument contains a pandas-dataframe containing the local
     data from the node.
     """
-    # extract the column_name from the dataframe.
-    info(f"Extracting column {column_name}")
-    values = df[column_name]
+    # count rows
+    num_rows = df.shape[0]
 
-    # count number of rows
-    # TODO: this is just the uber-basic example, the skeleton.
-    info("Computing partials")
-    local_count = len(values)
+    # verify it's an integer
+    assert isinstance(num_rows, int)
 
     # return the values as a dict
     return {
-        "count": local_count
+        "count": num_rows
     }
